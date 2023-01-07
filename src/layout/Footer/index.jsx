0ab13223b1e8e6c2bb3e5dddf2c65208ba886269
saber nikfar <nikfar.saber@gmail.com>
2023-01-07 14:00:21 +0330

    complete jsx file in Footer component

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -1,7 +1,50 @@
 import styles from "./footer.module.css";
 
+import enamadIcon from "../../assets/pngFolder/enamad.png";
+import logo from "../../assets/pngFolder/safeservice-logo.png";
+import { CallSvg, LocationSvg, MailSvg } from "../../assets/SvgInput";
+import SocialMediaRef from "../../component/SocialMediaRef";
+import Button from "../../UI/Button";
+
 const Footer = () => {
-  return <footer>footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.upSection}>
+        <img src={logo} alt="safeservice logo" className={styles.logo} />
+        <ul className={styles.aboutUs}>
+          <li>تماس با ما</li>
+          <li>درباره ما</li>
+          <li>اخبار سیف سرویس</li>
+        </ul>
+        <ul className={styles.contactUs}>
+          <li>
+            <LocationSvg color={"white"} />
+            <p>
+              خیابان ولیعصر پایین‌تر از پارک ساعی روبروی هتل سیمرغ پلاک ۲۲۰۸
+              طبقه منفی ۱
+            </p>
+          </li>
+          <li>
+            <CallSvg color={"white"} />
+            <p>۰۲۱-۷۱۷۵۸ ۷روز هفته، ۲۴ ساعته پاسخگوی شما هستیم</p>
+          </li>
+          <li>
+            <MailSvg color={"white"} />
+            <p>info@safeservice.co</p>
+          </li>
+          <li>
+            <p>شبکه های اجتماعی:</p>
+            <SocialMediaRef />
+          </li>
+        </ul>
+      </div>
+      <div className={styles.downSection}>
+        <input type="text" />
+        <Button which="confirm" text="ثبت" importStyle={{ fontSize: "20px" }} />
+      </div>
+      <img src={enamadIcon} alt="enamad logo" />
+    </footer>
+  );
 };
 
 export default Footer;
