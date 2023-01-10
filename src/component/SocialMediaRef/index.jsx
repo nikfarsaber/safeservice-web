1a1dd71b0f7f7ff0d0153a57daac753cb9991c10
saber nikfar <nikfar.saber@gmail.com>
2023-01-10 12:08:08 +0330

    update and add ability to change icon color in SocialMediaRef component

--- src/component/SocialMediaRef/index.jsx
+++ src/component/SocialMediaRef/index.jsx
@@ -2,12 +2,12 @@ import { TelegramSvg, FacebookSvg, InstagramSvg } from "../../assets/SvgInput";
 
 import styles from "./socialMediaRef.module.css";
 
-const SocialMediaRef = ({ className }) => {
+const SocialMediaRef = ({ className, color = "white" }) => {
   return (
     <div className={`${className} ${styles.socialMediaRef}`}>
-      <InstagramSvg className={styles.svgIcon} color="white" />
-      <FacebookSvg className={styles.svgIcon} color="white" />
-      <TelegramSvg className={styles.svgIcon} color="white" />
+      <InstagramSvg className={styles.svgIcon} color={color} />
+      <FacebookSvg className={styles.svgIcon} color={color} />
+      <TelegramSvg className={styles.svgIcon} color={color} />
     </div>
   );
 };
