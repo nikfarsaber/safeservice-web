eb927a3ccb583d7b7a20fcd12078be0bdc46f428
saber nikfar <nikfar.saber@gmail.com>
2023-01-10 11:59:32 +0330

    update Input component and add important and direction and black color

--- src/UI/Input/index.jsx
+++ src/UI/Input/index.jsx
@@ -1,13 +1,21 @@
 import styles from "./input.module.css";
 
-const Input = ({ text, type, placeholder, which, className }) => {
+const Input = ({
+  text,
+  type,
+  placeholder,
+  which,
+  className,
+  important,
+  direction,
+}) => {
   return (
     <>
-      <label className={`${styles[which]} ${className} ${styles.label}`}>
+      <label className={`${styles[which]} ${className} ${styles.label} ${important && styles.important}`}>
         {text}
       </label>
       <input
-        className={`${styles[which]} ${className} ${styles.input}`}
+        className={`${styles[which]} ${className} ${styles.input} ${styles[direction]}`}
         type={type}
         placeholder={placeholder}
       />
