0be355fbbe72f2d7735f62d61dd0c2394b7f15eb
saber nikfar <nikfar.saber@gmail.com>
2023-01-10 14:45:44 +0330

    add useState hook for check wantRegister or signin for change UI and complate UI change

--- src/pages/Authentication/index.jsx
+++ src/pages/Authentication/index.jsx
@@ -1,62 +1,78 @@
 import Input from "../../UI/Input";
 import Button from "../../UI/Button";
-import { Link } from "react-router-dom";
 import SocialMediaRef from "../../component/SocialMediaRef";
 
 import styles from "./authentication.module.css";
 
 import logo from "../../assets/pngFolder/safeservice-logo.png";
+import { useState } from "react";
 
 const Authentication = () => {
+  const [wantRegister, setAreRegister] = useState(true);
+
+  const ToggleRegister = () => {
+    const updateWantRegister = !wantRegister;
+    setAreRegister(updateWantRegister);
+  };
+
   return (
     <div className={styles.page}>
       <div className={styles.informationBox}>
         <img src={logo} alt="safeservice logo" className={styles.logo} />
-        <Input
-          className={styles.input}
-          which="black"
-          text="نام و نام خانوادگی"
-          type="text"
-          important={true}
-          placeholder="لطفا نام و نام خانوادگی خود را وارد کنید."
-        />
+        {wantRegister && (
+          <Input
+            className={styles.input}
+            which="black"
+            text="نام و نام خانوادگی"
+            type="text"
+            important={true}
+            placeholder="لطفا نام و نام خانوادگی خود را وارد کنید."
+          />
+        )}
         <Input
           direction="left"
           className={styles.input}
           which="black"
           text="رمز عبور"
           type="password"
-          important={true}
+          important={wantRegister}
           placeholder="****"
         />
-        <Input
-          direction="left"
-          className={`${styles.input} ${styles.passwordCheck}`}
-          which="black"
-          text="تایید رمز عبور"
-          type="password"
-          important={true}
-          placeholder="****"
-        />
+        {wantRegister && (
+          <Input
+            direction="left"
+            className={`${styles.input}`}
+            which="black"
+            text="تایید رمز عبور"
+            type="password"
+            important={true}
+            placeholder="****"
+          />
+        )}
         <label className={styles.remainingCheck}>
           <input type="checkbox" />
           مرا به خاطر بسپار
         </label>
         <Input
           direction="left"
-          className={styles.input}
+          className={`${styles.input} ${!wantRegister && styles.goFirst}`}
           which="black"
           text="شماره تلفن همراه"
           type="text"
-          important={true}
+          important={wantRegister}
           placeholder="09xxxxxxxxx"
         />
-        <Button className={styles.button} which="confirm" text="ثبت نام" />
+        <Button
+          className={styles.button}
+          which="confirm"
+          text={wantRegister ? "ثبت نام" : "ورود"}
+        />
         <p className={styles.haveAccount}>
+          {wantRegister ? "حساب کاربری دارید؟ " : "ساختن حساب کاربری جدید"}
           حساب کاربری دارید؟{" "}
-          <Link className={styles.goToSignin} to="/signin">
-            ورود
-          </Link>
+          <span className={styles.goToSignin} onClick={ToggleRegister}>
+            {wantRegister ? "ورود" : "ثبت نام"}
+          </span>
         </p>
         <hr className={styles.dividerLine} />
         <div className={styles.socialMediaBox}>
