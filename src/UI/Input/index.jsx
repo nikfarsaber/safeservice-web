c332eedf37f6e127f86977f83c646afa5a491a0a
saber nikfar <nikfar.saber@gmail.com>
2023-01-13 00:50:55 +0330

    add check valid for Input component by useState after first touch or blur input

--- src/UI/Input/index.jsx
+++ src/UI/Input/index.jsx
@@ -1,4 +1,5 @@
 import styles from "./input.module.css";
+import { useState } from "react";
 
 const Input = ({
   text,
@@ -8,17 +9,44 @@ const Input = ({
   className,
   important,
   direction,
+  validation = () => true,
+  errorText = "value is NOT valid",
 }) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  const valueIsValid = validation && validation(enteredValue);
+  if (hasError !== !valueIsValid && isTouched) {
+    setHasError(!valueIsValid && isTouched);
+  }
+
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const inputBlurHandler = () => {
+    setIsTouched(true);
+  };
+
   return (
     <>
-      <label className={`${styles[which]} ${className} ${styles.label} ${important && styles.important}`}>
+      <label
+        className={`${styles[which]} ${className} ${styles.label} ${
+          important && styles.important
+        }`}
+      >
         {text}
       </label>
       <input
+        onChange={valueChangeHandler}
+        onBlur={inputBlurHandler}
         className={`${styles[which]} ${className} ${styles.input} ${styles[direction]}`}
         type={type}
         placeholder={placeholder}
+        value={enteredValue}
       />
+      {hasError && <p>{errorText}</p>}
     </>
   );
 };
