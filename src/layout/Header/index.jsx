dfa74592164260353c3916b50ae7d90653bee1ff
saber nikfar <nikfar.saber@gmail.com>
2023-01-25 03:48:31 +0330

    update header and add sign out when token exist in local storage

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,6 +1,7 @@
 import NavBar from "../../component/NavBar";
 import Button from "../../UI/Button";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 import styles from "./header.module.css";
 
@@ -8,8 +9,14 @@ import logo from "../../assets/pngFolder/safeservice-logo.png";
 
 const Header = ({ marginInline, className }) => {
   const navigate = useNavigate();
+  const [isLogIn, setIsLogIn] = useState(localStorage.getItem("userToken"));
   const logInButtonHandler = () =>
     navigate("/authentication", { replace: true });
+
+  const logOutBottonHandler = () => {
+    localStorage.removeItem("userToken");
+    setIsLogIn("");
+  };
 
   return (
     <header
@@ -18,12 +25,21 @@ const Header = ({ marginInline, className }) => {
     >
       <div className={styles.rightSide}>
         <img className={styles.logo} src={logo} alt="safeservice logo" />
-        <Button
-          text="ورود/ثبت نام"
-          which="register"
-          className={styles.loginButton}
-          onClick={logInButtonHandler}
-        />
+        {!isLogIn ? (
+          <Button
+            text="ورود/ثبت نام"
+            which="register"
+            className={styles.loginButton}
+            onClick={logInButtonHandler}
+          />
+        ) : (
+          <Button
+            text="خروج از حساب کاربری"
+            which="register"
+            className={styles.loginButton}
+            onClick={logOutBottonHandler}
+          />
+        )}
         <NavBar fontSize="20px" spaceBetween="24px" />
       </div>
       <div className={styles.leftSide}>
