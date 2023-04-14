18e3a7e60ca0c92cc29123ac550cdeb02f78bcd9
saber nikfar <nikfar.saber@gmail.com>
2023-04-14 17:59:19 +0330

    update productCard.jsx UI

--- src/UI/ProductCard/index.jsx
+++ src/UI/ProductCard/index.jsx
@@ -16,24 +16,38 @@ const ProductCard = ({
 
   return (
     <div className={styles.productCard}>
-      <img src={imgUrl} alt={productName} className={styles.productImg} />
-      <p className={styles.productName}>{productName}</p>
-      {withoutPrice && offerPercent && (
-        <div className={styles.offerRow}>
-          <p className={styles.initialPrice}>
-            {withoutPrice.toString().replace(/[0-9]/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[d])}
-          </p>
-          <p className={styles.offerPercent}>
-            {offerPercent.toString().replace(/[0-9]/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[d])}
-          </p>
-        </div>
-      )}
+      <div className={styles.topProductCard}>
+        <img src={imgUrl} alt={productName} className={styles.productImg} />
+        <p className={styles.productName}>{productName}</p>
+        {withoutPrice && offerPercent && (
+          <div className={styles.offerRow}>
+            <p className={styles.initialPrice}>
+              {new Intl.NumberFormat()
+                .format(withoutPrice)
+                .toString()
+                .replace(/[0-9]/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[d])}{" "}
+              تومان
+            </p>
+            <p className={styles.offerPercent}>
+              {offerPercent
+                .toString()
+                .replace(/[0-9]/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[d])}
+            </p>
+          </div>
+        )}
+      </div>
       <div className={styles.productBottom}>
-        <HeartSvg onClick={clickHeartHandler} color="#292D32" />
-        <BasketSvg onClick={clickBasketHandler} color="#292D32" />
+        <div className={styles.quickAccess}>
+          <HeartSvg onClick={clickHeartHandler} color="#292D32" />
+          <BasketSvg onClick={clickBasketHandler} color="#292D32" />
+        </div>
         {finalPrice && (
           <p className={styles.finalPrice}>
-            {finalPrice.toString().replace(/[0-9]/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[d])}
+            {new Intl.NumberFormat()
+              .format(finalPrice)
+              .toString()
+              .replace(/[0-9]/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[d])}{" "}
+            تومان
           </p>
         )}
       </div>
